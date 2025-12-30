8d2d2e8976e9be640a064ffb04d901bf1778c5d9
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2025-12-30 15:31:45 -0300

    Update hyprland config

--- hypr/scripts/scratchpad.sh
+++ hypr/scripts/scratchpad.sh
@@ -1,20 +1,26 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Set default names for the class and the workspace
-CLASS_NAME="mainscratch"
+CLASS_NAME="com.mainpad"
 WORKSPACE_NAME="main"
+CMD=""
 
 # If --alt is passed, use the alternative names
 if [[ "$1" == "--alt" ]]; then
-    CLASS_NAME="altscratch"
+    CLASS_NAME="com.altpad"
     WORKSPACE_NAME="alt"
+elif [[ "$1" == "--zk" ]]; then
+    CLASS_NAME="com.zkpad"
+    WORKSPACE_NAME="zk"
+    CMD="zk z"
 fi
 
 # Check if a window with the specific class already exists anywhere
 if hyprctl clients | grep -q "class: $CLASS_NAME"; then
-    # If it exists, toggle the corresponding named special workspace
     hyprctl dispatch togglespecialworkspace $WORKSPACE_NAME
 else
-    # Otherwise, execute Alacritty on the corresponding named special workspace
-    hyprctl dispatch exec "[workspace special:$WORKSPACE_NAME] alacritty --class $CLASS_NAME"
+    hyprctl dispatch exec "[workspace special:$WORKSPACE_NAME] \
+      ghostty \
+      --class=$CLASS_NAME \
+      --command='$CMD'"
 fi
