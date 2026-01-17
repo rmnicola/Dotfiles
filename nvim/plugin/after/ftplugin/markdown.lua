d18f2d31e1149b642c1f7bbe73ec55770fa3a147
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2026-01-17 14:53:05 -0300

    Update markdown filetype spec

--- nvim/plugin/after/ftplugin/markdown.lua
+++ nvim/plugin/after/ftplugin/markdown.lua
@@ -1,5 +1,16 @@
--- Set conceallevel to 0 (show everything) when entering a markdown file.
--- If you later toggle "Render Markdown" (<leader>um) ON,
--- that plugin will automatically increase this level to hide things again.
+-- nvim/after/ftplugin/markdown.lua
+
+-- 1. Set conceallevel (your existing config)
 vim.opt_local.conceallevel = 0
+
+-- 2. Create an autocommand to disable diagnostics *whenever* an LSP attaches
+--    This catches nvim-zk, markdownlint, and any others that load late.
+vim.api.nvim_create_autocmd("LspAttach", {
+  buffer = 0, -- Apply only to the current markdown buffer
+  callback = function()
+    vim.diagnostic.enable(false, { bufnr = 0 })
+  end,
+})
+
+-- 3. Just in case diagnostics were already loaded (belt and suspenders)
 vim.diagnostic.enable(false, { bufnr = 0 })
