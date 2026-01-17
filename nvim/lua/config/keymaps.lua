b5abc09c60ac44fea71859bc14aeec45e6db11d9
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2026-01-17 14:52:31 -0300

    Update nvim keymaps and options

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set -- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Default options that are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = true
-vim.opt.autoread = true
 vim.g.minipairs_disable = true
+vim.opt.autoread = true
+vim.opt.backup = false
+vim.opt.belloff = "all"
+vim.opt.breakindent = true
+vim.opt.colorcolumn = "80"
+vim.opt.complete = ".,w,b,u,t"
+vim.opt.completeopt = "menuone,noinsert,noselect,preview"
+vim.opt.expandtab = true
 vim.opt.laststatus = 0
+vim.opt.makeprg = "make"
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
 vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.textwidth = 80
