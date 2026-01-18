537f74ac01a43ec597b1c8bada486b3bc48ec86b
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2026-01-18 19:19:43 -0300

    Remove option to disable minipairs
    Already disabling the entire plugin

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,6 @@
 -- Default options that are always set:
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.minipairs_disable = true
 local opt = vim.opt
 opt.autoread = true
 opt.autowrite = false
