ac2c91e22f456f36638f985799a5c71d32aeffe7
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2026-02-17 19:38:27 -0300

    Remove tex plugin nvim

--- nvim/lua/snippets/presenterm.lua
+++ nvim/lua/snippets/presenterm.lua
@@ -6,7 +6,7 @@ luasnip.add_snippets("markdown", {
   s("ptcenter", { t("<!-- jump_to_middle -->"), }),
   s("ptfontsize", { t("<!-- font_size: 2 -->"), }),
   s("ptinclist", { t("<!-- incremental_lists: true -->"), }),
-  s("ptimg", { t("![image:width:90%](./assets/)"), }),
+  s("ptimg", { t("![image:width:90%](../assets/)"), }),
   s("ptitemlines", { t("<!-- list_item_newlines: 1 -->"), }),
   s("ptnewlines", { t("<!-- new_lines: 1 -->"), }),
   s("ptskip", { t("<!-- skip_line -->"), }),
