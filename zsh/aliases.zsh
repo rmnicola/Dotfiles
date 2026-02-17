2945d847cba45f0c19ee0327edd608aa89d5098b
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2026-02-17 19:39:12 -0300

    Add opencode config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -38,5 +38,5 @@ alias gp="git push"
 alias gc="git commit"
 
 # Opencode
-alias ocp='OPENCODE_CONFIG_DIR=~/.config/opencode opencode'
-alias oci='OPENCODE_CONFIG_DIR=~/.config/opencode-inteli opencode'
+alias ocp='OPENCODE_CONFIG_DIR=~/.config/opencode-personal opencode --port'
+alias oci='OPENCODE_CONFIG_DIR=~/.config/opencode opencode --port'
